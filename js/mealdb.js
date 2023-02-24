27dad5b4f84c22da1663f33db47b8ecf4303a163
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-02-24 15:01:35 +0600

    first commit

---
+++ js/mealdb.js
@@ -0,0 +1,44 @@
+const loadData = async (searchText) => {
+    console.log(searchText);
+    try {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+        console.log(url);
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data.meals);
+        displayData(data.meals);
+
+    }
+    catch (eror) {
+        console.log(console.error)
+    }
+
+}
+
+// display data
+displayData = (meals) => {
+    console.log(meals);
+    const mealsContainer = document.getElementById("meals-container");
+    meals.forEach(meal => {
+        console.log(meal);
+        const everyMealDiv = document.createElement("div");
+        everyMealDiv.innerHTML = `
+        <div class="card w-[80%] card-side bg-base-100 shadow-xl mx-auto">
+           <figure><img class="w-60" src="${meal.strMealThumb}" /></figure>
+           <div class="card-body">
+                <h2 class="card-title">${meal.strMeal}</h2>
+                <p>Click the button to watch on netflix app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Details</button>
+              </div>
+            </div>
+        </div>
+        `
+        mealsContainer.appendChild(everyMealDiv);
+    });
+
+
+}
+
+// loadData
+loadData("chicken");
